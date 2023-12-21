f2365e0b7588a3c5b028e8f71ba104254874819e
Harold-coder <hjc2154@columbia.edu>
2023-12-21 23:28:27 +0100

    changed to json

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, request, jsonify
 import psycopg2
 from datetime import datetime
 from graphene import ObjectType, String, List, Schema
 from flask_graphql import GraphQLView
 
 app = Flask(__name__)
-
 
 # Database connection function
 def get_db_connection():
@@ -17,12 +16,7 @@ def get_db_connection():
         database='lion_leftovers'
     )
 
-
-@app.route("/")
-def index():
-    return render_template('index.html')
-
-#GRAPHQL
+# GraphQL setup
 class MealType(ObjectType):
     inventory_id = String()
     dining_hall_id = String()
@@ -30,7 +24,6 @@ class MealType(ObjectType):
     quantity = String()
     price = String()
     expiration_time = String()
-# GraphQL Query
 
 class Query(ObjectType):
     all_meals = List(MealType)
@@ -59,34 +52,43 @@ class Query(ObjectType):
 # Setup GraphQL Schema
 schema = Schema(query=Query)
 
+# Adding GraphQL route
+app.add_url_rule(
+    '/graphql',
+    view_func=GraphQLView.as_view(
+        'graphql',
+        schema=schema,
+        graphiql=True  # Enables GraphiQL interface
+    )
+)
+
 @app.route('/view_meals_student')
 def view_meals_student():
-    return render_template('view_meals_student.html')
-@app.route('/view_meals_worker')
-def view_meals_worker():
-    page = request.args.get('page', 1, type=int)
-    per_page = request.args.get('per_page', 10, type=int)
-    dining_hall_id = request.args.get('dining_hall_id', default=None, type=str)
-    inventory_id = request.args.get('inventory_id', default=None, type=int)
-
     conn = get_db_connection()
     cursor = conn.cursor()
+    # Get the current time
+    current_time = datetime.now()
 
-    if inventory_id:
-        cursor.execute("SELECT * FROM Inventory WHERE InventoryID = %s LIMIT %s OFFSET %s",
-                       (inventory_id, per_page, (page - 1) * per_page))
-    elif dining_hall_id:
-        cursor.execute("SELECT * FROM Inventory WHERE DiningHallID = %s LIMIT %s OFFSET %s",
-                       (dining_hall_id, per_page, (page - 1) * per_page))
-    else:
-        cursor.execute("SELECT * FROM Inventory LIMIT %s OFFSET %s", (per_page, (page - 1) * per_page))
-
+    # Modify the SQL query to only select non-expired meals
+    query = """
+    SELECT * FROM Inventory 
+    WHERE ExpirationTime > %s
+    """
+    cursor.execute(query, (current_time,))
     inventory_items = cursor.fetchall()
     conn.close()
 
-    # Pass the paginated inventory items to the template
-    return render_template('view_meals_worker.html', inventory=inventory_items, page=page, per_page=per_page)
+    meals = [
+        {
+            "inventory_id": item[0],
+            "dining_hall_id": item[1],
+            "food_item": item[2],
+            # ... add other fields as needed
+        }
+        for item in inventory_items
+    ]
 
+    return jsonify(meals)
 
 @app.route('/manage_inventory', methods=['GET', 'POST'])
 def manage_inventory():
@@ -97,111 +99,71 @@ def manage_inventory():
         action = request.form.get('action')
         inventory_id = request.form.get('inventory_id')
 
-        try:
-            if action == 'add' or action == 'update':
-                # Fetching form fields common to both add and update actions
-                dining_hall_id = request.form.get('dining_hall_id')
-                food_item = request.form.get('food_item')
-                quantity = request.form.get('quantity', type=int)
-                price = request.form.get('price', type=float)
-                expiration_time = request.form.get('expiration_time')
-
-                # Convert expiration_time from string to datetime
-                expiration_time = datetime.strptime(expiration_time, '%Y-%m-%dT%H:%M') if expiration_time else None
-
-                if action == 'add':
-                    # SQL query to add a new inventory item
-                    cursor.execute(
-                        "INSERT INTO Inventory (DiningHallID, FoodItem, Quantity, Price, ExpirationTime) VALUES (%s, %s, %s, %s, %s)",
-                        (dining_hall_id, food_item, quantity, price, expiration_time))
-                    conn.commit()
-                elif action == 'update':
-                    # SQL query to update an existing inventory item
-                    cursor.execute(
-                        "UPDATE Inventory SET DiningHallID=%s, FoodItem=%s, Quantity=%s, Price=%s, ExpirationTime=%s WHERE InventoryID=%s",
-                        (dining_hall_id, food_item, quantity, price, expiration_time, inventory_id))
-                    conn.commit()
-
-            elif action == 'delete':
-                # SQL query to delete an inventory item
-                cursor.execute("DELETE FROM Inventory WHERE InventoryID=%s", (inventory_id,))
-                conn.commit()
-
-            conn.commit()
-
-        except Exception as e:
-            print("An error occurred:", e)
-            conn.rollback()
-
     cursor.execute("SELECT * FROM Inventory")
     inventory = cursor.fetchall()
     conn.close()
 
-    formatted_inventory = []
-    for item in inventory:
-        formatted_item = list(item)
-        # Format datetime fields
-        if formatted_item[5]:  # Assuming this is the ExpirationTime field
-            formatted_item[5] = formatted_item[5].strftime('%Y-%m-%dT%H:%M')
-        formatted_inventory.append(formatted_item)
+    formatted_inventory = [
+        {
+            "inventory_id": item[0],
+            "dining_hall_id": item[1],
+            "food_item": item[2],
+            # ... add other fields as needed
+        }
+        for item in inventory
+    ]
 
-    return render_template('manage_inventory.html', inventory=formatted_inventory)
-
+    return jsonify(formatted_inventory)
 
 @app.route("/view_meals_student/<string:dining_hall_id>")
 def meals_by_dining_hall(dining_hall_id):
-    current_datetime = datetime.now()
-
     conn = get_db_connection()
     cursor = conn.cursor()
     query = """
         SELECT * FROM Inventory 
         WHERE DiningHallID = %s 
         AND expirationtime > %s
-        """
-    cursor.execute(query, (dining_hall_id, current_datetime))
+    """
+    cursor.execute(query, (dining_hall_id, datetime.now()))
     meals = cursor.fetchall()
     conn.close()
-    return render_template('meals_by_dining_hall.html', meals=meals, dining_hall_id=dining_hall_id)
 
+    formatted_meals = [
+        {
+            "inventory_id": meal[0],
+            "dining_hall_id": meal[1],
+            "food_item": meal[2],
+            # ... add other fields as needed
+        }
+        for meal in meals
+    ]
 
+    return jsonify(formatted_meals)
 
 @app.route("/view_meals_student/<int:inventory_id>")
 def inventory_item(inventory_id):
     conn = get_db_connection()
     cursor = conn.cursor()
-
-    current_datetime = datetime.now()  # Get the current date and time
-
     query = """
             SELECT * FROM Inventory 
             WHERE InventoryID = %s 
             AND expirationtime > %s
-            """
-
-    # Provide both inventory_id and current_datetime as parameters
-    cursor.execute(query, (inventory_id, current_datetime))
+    """
+    cursor.execute(query, (inventory_id, datetime.now()))
     item = cursor.fetchone()
-
-    cursor.close()
     conn.close()
 
     if item is None:
-        return "Inventory item not found", 404
+        return jsonify({"error": "Inventory item not found"}), 404
 
-    print(item)
+    formatted_item = {
+        "inventory_id": item[0],
+        "dining_hall_id": item[1],
+        "food_item": item[2],
+        # ... add other fields as needed
+    }
 
-    return render_template('inventory_item.html', item=item)
-
-# Adding GraphQL route
-app.add_url_rule(
-    '/graphql',
-    view_func=GraphQLView.as_view(
-        'graphql',
-        schema=schema,
-        graphiql=True  # Enables GraphiQL interface
-    )
-)
+    return jsonify(formatted_item)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8012)
